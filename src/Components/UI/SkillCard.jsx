8b129b3e0a372729055481107c346200f5c49ad5
Ajit Sharma <ajitsharma2021@gmail.com>
2023-08-10 20:16:22 +0530

    Initial Files Added

--- src/Components/UI/SkillCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
-
-const SkillCard = () => {
-  return <div>SkillCard</div>;
-};
-
-export default SkillCard;
